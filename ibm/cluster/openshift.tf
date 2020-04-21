4b1d45bae456cb17190d7f88e93db1a2ddb5b78a
Unknown <juliansubda@gmail.com>
2020-04-21 08:49:54 -0700

    Untested settings while waiting to verify on manual deploy

--- ibm/cluster/openshift.tf
+++ ibm/cluster/openshift.tf
@@ -1,10 +1,10 @@
 resource "ibm_container_cluster" "cluster" {
   name              = "bcgov-ocp"
-  datacenter        = "dal10"
+  datacenter        = "wdc10"
   default_pool_size = 3
   machine_type      = "b3c.4x16"
   hardware          = "shared"
-  kube_version      = "3.11_openshift"
+  kube_version      = "4.3.2_openshift"
   public_vlan_id    = "<public_vlan_ID>"
   private_vlan_id   = "<private_vlane_ID>"
   lifecycle {
