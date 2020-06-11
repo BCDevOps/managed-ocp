76dd384f80c7666401b67b354419d30a7bf22084
Derek Roberts <derek.roberts@gmail.com>
2020-06-11 13:41:36 -0700

    Add halt conditions to ibm prereqs

--- platforms/ibm/prerequisites.sh
+++ platforms/ibm/prerequisites.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -euo pipefail
+
 CALLER_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd );
 source "$CALLER_DIR/../../lib/dependency_tools_install.sh";
 source "$CALLER_DIR/../../lib/setup_helper.sh";
